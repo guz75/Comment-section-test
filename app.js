0234a5ceba2735fbb48a60c12e7503a725e6835a
Guy Slator <guyslator@hotmail.co.uk>
2020-06-06 16:28:36 +0100

    Initial styling done

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get("/", function(req, res){
+  res.render("main");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.fullName);
+});
 
 app.listen(3000, function(){
   console.log("Server connected on port 3000");
