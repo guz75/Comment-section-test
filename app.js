ad0a2162c36ed71f9caf0498fd208397f9f94760
Guy Slator <guyslator@hotmail.co.uk>
2020-06-07 16:45:51 +0100

    Comments go to preview. Database is working.

--- app.js
+++ app.js
@@ -11,14 +11,49 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/commentsDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  website: String,
+  title: String,
+  comment: String
+});
+
+const Post = mongoose.model("Posts", postSchema);
+const Preview = mongoose.model("Preview", postSchema);
 
 app.get("/", function(req, res){
-  res.render("main");
+  Post.find({}, function(err, posts){
+    if(!err){
+      res.render("main", {posts: posts});
+    }
+  });
+
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.fullName);
+  const post = new Preview({
+    name: req.body.name,
+    email:req.body.email,
+    website: req.body.website,
+    title: req.body.title,
+    comment: req.body.comment
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/preview");
+    }
+  });
+});
+
+app.get("/preview", function(req, res){
+  Preview.find({}, function(err, posts){
+    if(!err){
+      res.render("preview", {posts: posts});
+    }
+  });
 });
 
 app.listen(3000, function(){
